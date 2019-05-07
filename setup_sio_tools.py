0e142072033b4c2c294f1768388aedde8312e807
SergeDmi <serge.dmitrieff@gmail.com>
2019-05-07 10:58:08 +0200

    sio_tools and seplot separate

---
+++ setup_sio_tools.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from sio_tools import sio_tools as sio
+
+version=sio.__VERSION__
+
+setup(
+     name='sio_tools',
+     version=version,
+
+     author="Serge Dmitrieff",
+     description="I/O tools for seplot and other python projects",
+     url="https://github.com/SergeDmi/Python-Tools/",
+     install_requires=[
+          'numpy'
+      ],
+     packages=['sio_tools' ]
+ )
