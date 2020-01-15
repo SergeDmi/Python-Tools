f3f83d2d4d8c6d73e7327df18ceeca58e32b8685
SergeDmi <serge.dmitrieff@gmail.com>
2020-01-15 17:05:57 +0100

    added dictionaries for styles and keywords

---
+++ seplot/kw_dictionaries.py
@@ -0,0 +1,16 @@
+def get_keywords():
+
+    kw_dict={
+        'function'          : 'function_string',
+        'title'             : 'legend',
+        'andcond'           : 'cond',
+        'if'                : 'cond',
+        'andif'             : 'cond',
+        'color'             : 'col',
+        'size'              : 'siz',
+        'style'             : 'stil',
+        'npoints'           : 'n_points',
+        'npts'              : 'n_points'
+    }
+
+    return kw_dict
