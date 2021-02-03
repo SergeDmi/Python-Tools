b0f61801e635b4dbee5e83b006005ba44bc2bccf
dmitrief <serge.dmitrieff@gmail.com>
2021-02-03 09:21:00 +0100

    added intermediate script

---
+++ seplot/seploter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright Serge Dmitrieff
+# www.biophysics.fr
+#
+# Based on Python Pyx
+import sys
+#from seplot.seplot import Splotter
+
+
+
+if __name__ == "__main__":
+    nargs=len(sys.argv)
+    dargs=sys.argv[1:]
+    print(sys.version)
+    #seplot=Splotter(arguments=dargs)
+    #seplot.make_and_save()
